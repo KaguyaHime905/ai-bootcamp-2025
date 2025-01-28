0ebdf35dc29fcd28bea78bd3e1966c80de734663
KaguyaHime905 <giuliagambuzza1@gmail.com>
2025-01-28 10:11:53 +0100

    esercizio giorno 1 giulia gambuzza

--- python/giorno1/esercizio/main.py
+++ python/giorno1/esercizio/main.py
@@ -1,80 +1,81 @@
-print(Inizio programma)
+print("Inizio programma")
 
 # Assegno la variabile foo
-False = foo
+foo = False
 
 # Questi controlli assert devono passare tutti
-assert bool(0)
-assert False == True
-assert True is False
-assert True != True
-assert None is False
+assert bool(1)
+assert False != True
+assert True is True
+assert True == True
+assert None is None
 
 # Faccio alcune operazioni aritmetiche sui numeri interi
 bar = 0
 baz = 1
-result = baz / bar
+result = bar/baz
+print(result)       #0.0
 
 # Incremento il risultato di uno
-
-result + 1
+result += 1
+print(result)       #1.0
 
 # Decremento il risultato di uno
-
 result -= 1
+print(result)       #0.0
 
 # Controllo che il valore non sia negativo
-assert result < 0
+assert result >= 0      #NON è NEGATIVO, MA UGUALE A 0
 
 # Concateno le stringhe
-message = "hello" + b"world"
+message = "hello" + "world"
+print(message)    #helloworld
 
 # Creo una lista e la estendo
 li1 = [1, 2]
-li1 += [3],
+li1.append(3)
+print(li1)    #[1, 2, 3]
 
-# Non mi ricordo come si "prepende" un valore...
-li1 = ...
+# Non mi ricordo come si "prepende" un valore...        ???
+li1.insert(0,0)     #mette il numero 0 nella posizione 0
 
 # Verifico che il risultato sia quello che mi aspetto
 assert li1 == [0, 1, 2, 3]
 
 # Creo una tupla e la estendo
 tu1 = (1, 2)
-tu1 += (3)
+tu2 = tu1 + (3,)    #creo una nuova tupla con i valori di tu1, aggiungendo 3
 
-assert tu1 == (1, 2, 3)
+assert tu2 == (1, 2, 3)
 
 # Creo un dict
-
 d1 = {}
-d2["a"] = 1
-d1[b] = 2
-
+d1["a"] = 1
+d1["b"] = 2
 assert d1["a"] == 1
 assert d1 == {"a": 1, "b": 2}
 
 # Cancello la chiave "b"
-del d1[b]
+del d1["b"]
 
 # Controllo che il dict non contenga ancora la chiave "b"
-assert "b" in d1
+assert "b" not in d1        #True
 
-# Potrei anche controllarlo in questo modo
-# e verificare anche la presenza di "a"
-if "b" not in "b":
-    assert False
-elif "a":
+# Potrei anche controllarlo in questo modo e verificare anche la presenza di "a"
+if "b" not in d1:
+    assert True
+elif "a" in d1:
     assert False
 else:
     assert True
 
 # Stampo la scritta "Ciao" tre volte poi esco
 # Conto le volte che l'ho stampata
-count = 0
-for idx in [1, 2]:
+count=0
+for idx in [0, 1, 2]:
     count += 1
-    print("Ciao")
+    print(count,"Ciao") #conteggio singolo e stampa
+print('Il totale delle stampe è:',count)    #conteggio totale delle stampe: 3
 
 # Controllo che l'abbia stampata tre volte
 assert count == 3
@@ -83,8 +84,9 @@ assert count == 3
 num = 3
 while num > 0:
     print("Ciao")
-
+    num -= 1
 print("Fine programma")
 
 # Bonus: verifico la seguente operazione sui float
-#assert 0.1 + 0.2 == 0.3
+print(0.1 + 0.2)        #0.30000000000000004
+assert 0.1 + 0.2 == 0.30000000000000004   #si dovrebbe far riferimento al concetto di tolleranza
